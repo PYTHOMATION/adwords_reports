ef2c1a82ff4254462720b968014684c10cf31f7a
Martin Winkel <martin.winkel.pps@gmail.com>
2017-07-20 21:26:10 -0700

    removed tests on removed entities

--- tests/test_other_models.py
+++ tests/test_other_models.py
@@ -19,12 +19,8 @@ def test_close_variant():
     assert cv.standardized == "hott hu"
     assert cv.variations == {"hott'-hü", "hott'-hu", "hott hü", "hott hu"}
 
-    assert TextHandler("Äm-berg', Oberpfalz").standardized_city() == "Äm berg'"
-    assert TextHandler("Äm-be'rg, Oberpfalz").standardized_city(decode=True) == "Am berg"
-    assert TextHandler("Hagen (Westfalen)").standardized_city() == "Hagen"
-
     assert TextHandler.remove_forbidden_adwords_chars({"asd(s)ad", "{asd(s)ad"}) == {"asdsad"}
-    assert TextHandler.remove_double_space("    qweq asd  qqr") == "qweq asd qqr"
+    assert TextHandler.remove_double_white_space("    qweq asd  qqr") == "qweq asd qqr"
     assert TextHandler.remove_punctuation("madrid a gasteiz / vitoria") == "madrid a gasteiz vitoria"
     assert TextHandler.remove_forbidden_adwords_chars("madrid a gasteiz / vitoria") == "madrid a gasteiz / vitoria"
 
