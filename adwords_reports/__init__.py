bb84402df342ee13299d4c9a2ab93c2d26a2112d
Martin Winkel <martin.winkel.pps@gmail.com>
2018-02-20 01:34:34 -0800

    updated version string

--- adwords_reports/__init__.py
+++ adwords_reports/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 app_dir = os.path.dirname(__file__)
 app_dir_components = app_dir.split(os.sep)
 base_dir = os.sep.join(app_dir_components[:-1])
